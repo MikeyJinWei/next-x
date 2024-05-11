36c56810354843303f27cc4e214f907909f4b387
MIkeyJ <currygeigei@gmail.com>
2024-05-12 00:24:00 +0800

    feat(src/components/Sidebar.jsx): 建置 Sidebar 組件

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,25 @@
+import { SiCoinmarketcap } from "react-icons/si";
+import { HiHome } from "react-icons/hi";
+import Link from "next/link";
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="flex flex-col gap-4">
+      <Link href="/">
+        <SiCoinmarketcap className="w-16 h-16 p-3 hover:bg-neutral-100 rounded-xl cursor-pointer transition-all duration-300 ease-in-out" />
+      </Link>
+      <Link
+        href="/"
+        className="flex items-center w-fit gap-2 p-3 rounded-xl hover:bg-neutral-100 cursor-pointer transition-all duration-300 ease-in-out"
+      >
+        <HiHome className="w-7 h-7" />
+        <span className="font-bold hidden xl:inline">Home</span>
+      </Link>
+      <button className="hidden xl:inline w-48 h-9 mt-4 rounded-full text-white bg-indigo-400 hover:brightness-95 shadow-md cursor-pointer transition-all duration-300 ease-in-out">
+        Sign In
+      </button>
+    </div>
+  );
 };
 
 export default Sidebar;
